f8e557ade3444a9454b2bfefbdd284a518c7eed1
Nikita Korovin <25564136+Kitos87@users.noreply.github.com>
2025-04-01 09:40:47 +0300

    yeeeeeeeeeeeeeeeeeeeeeees

--- tasks/stl/korovin_n_qsort_batcher/src/ops_stl.cpp
+++ tasks/stl/korovin_n_qsort_batcher/src/ops_stl.cpp
@@ -119,15 +119,16 @@ void TestTaskSTL::OddEvenMerge(std::vector<BlockRange>& blocks) {
   }
   int buffer_size = max_block_len * 2;
   std::vector<std::vector<int>> buffers(p / 2, std::vector<int>(buffer_size));
+  std::vector<std::thread> threads;
+  threads.reserve(p / 2);
+  std::vector<bool> changed_local_vec((p + 1) / 2);
 
   for (int iter = 0; iter < max_iters; iter++) {
     bool changed_global = false;
-    std::vector<bool> changed_local_vec((p + 1) / 2, false);
-    std::vector<std::thread> threads;
-    threads.reserve(p / 2);
+    std::fill(changed_local_vec.begin(), changed_local_vec.end(), false);
+    threads.clear();
     for (int i = iter % 2; i + 1 < p; i += 2) {
       int pair_idx = i / 2;
-
       threads.emplace_back([&, i, pair_idx]() {
         bool changed_local = InPlaceMerge(blocks[i], blocks[i + 1], buffers[pair_idx]);
         changed_local_vec[pair_idx] = changed_local;
