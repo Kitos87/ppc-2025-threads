08f266954e136659ef0c7b5bcf0f0341290cd295
Nikita Korovin <25564136+Kitos87@users.noreply.github.com>
2025-04-02 20:41:30 +0300

    davai

--- tasks/stl/korovin_n_qsort_batcher/src/ops_stl.cpp
+++ tasks/stl/korovin_n_qsort_batcher/src/ops_stl.cpp
@@ -21,36 +21,26 @@ int TestTaskSTL::GetRandomIndex(int low, int high) {
 }
 
 void TestTaskSTL::QuickSort(std::vector<int>::iterator low, std::vector<int>::iterator high, int depth) {
-  while (std::distance(low, high) > 1) {
-    int n = static_cast<int>(std::distance(low, high));
-    int random_index = GetRandomIndex(0, n - 1);
-    int pivot = *(low + random_index);
+  if (std::distance(low, high) <= 1) {
+    return;
+  }
 
-    auto partition_iter = std::partition(low, high, [pivot](int elem) { return elem <= pivot; });
-    auto mid_iter = std::partition(low, partition_iter, [pivot](int elem) { return elem < pivot; });
+  int n = static_cast<int>(std::distance(low, high));
+  int random_index = GetRandomIndex(0, n - 1);
+  int pivot = *(low + random_index);
 
-    int max_depth = static_cast<int>(std::log2(ppc::util::GetPPCNumThreads())) + 1;
+  auto partition_iter = std::partition(low, high, [pivot](int elem) { return elem <= pivot; });
+  auto mid_iter = std::partition(low, partition_iter, [pivot](int elem) { return elem < pivot; });
 
-    if (depth < max_depth) {
-      int left_size = static_cast<int>(std::distance(low, mid_iter));
-      int right_size = static_cast<int>(std::distance(partition_iter, high));
-      // Запускаем поток только для меньшей части
-      if (left_size < right_size) {
-        std::thread left(QuickSort, low, mid_iter, depth + 1);
-        low = partition_iter;  // продолжаем сортировку правой части в текущем потоке
-        depth++;
-        left.join();
-      } else {
-        std::thread right(QuickSort, partition_iter, high, depth + 1);
-        high = mid_iter;
-        depth++;
-        right.join();
-      }
-    } else {
-      QuickSort(low, mid_iter, depth + 1);
-      QuickSort(partition_iter, high, depth + 1);
-      break;
-    }
+  int max_depth = static_cast<int>(std::log2(ppc::util::GetPPCNumThreads())) + 1;
+
+  if (depth < max_depth) {
+    std::thread left(QuickSort, low, mid_iter, depth + 1);
+    QuickSort(partition_iter, high, depth + 1);
+    left.join();
+  } else {
+    QuickSort(low, mid_iter, depth + 1);
+    QuickSort(partition_iter, high, depth + 1);
   }
 }
 
