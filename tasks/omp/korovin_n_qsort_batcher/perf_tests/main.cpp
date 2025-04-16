fe8c9513cbffb1e0e497fa2c111d4ed25e8278f7
Nikita Korovin <25564136+Kitos87@users.noreply.github.com>
2025-04-16 18:10:40 +0300

    fix

--- tasks/omp/korovin_n_qsort_batcher/perf_tests/main.cpp
+++ tasks/omp/korovin_n_qsort_batcher/perf_tests/main.cpp
@@ -1,20 +1,36 @@
 #include <gtest/gtest.h>
 
+#include <algorithm>
 #include <chrono>
 #include <cstdint>
 #include <memory>
 #include <numeric>
+#include <random>
 #include <vector>
 
 #include "core/perf/include/perf.hpp"
 #include "core/task/include/task.hpp"
 #include "omp/korovin_n_qsort_batcher/include/ops_omp.hpp"
 
+namespace {
+constexpr int kSize = 250000;
+constexpr unsigned int kSeed = 25;
+
+std::vector<int> GenerateRndArray(int size, unsigned int seed) {
+  std::mt19937 gen(seed);
+  std::uniform_int_distribution<int> dist(-1000, 1000);
+
+  std::vector<int> result(size);
+  for (auto &elem : result) {
+    elem = dist(gen);
+  }
+  return result;
+}
+}  // namespace
+
 TEST(korovin_n_qsort_batcher_omp, test_pipeline_run) {
-  constexpr int kSize = 250000;
-  std::vector<int> in(kSize);
+  std::vector<int> in = GenerateRndArray(kSize, kSeed);
   std::vector<int> out(in.size());
-  std::iota(in.rbegin(), in.rend(), 1);
 
   auto task_data_omp = std::make_shared<ppc::core::TaskData>();
   task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
@@ -38,13 +54,12 @@ TEST(korovin_n_qsort_batcher_omp, test_pipeline_run) {
   auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_omp);
   perf_analyzer->PipelineRun(perf_attr, perf_results);
   ppc::core::Perf::PrintPerfStatistic(perf_results);
+  ASSERT_TRUE(std::ranges::is_sorted(out.begin(), out.end()));
 }
 
 TEST(korovin_n_qsort_batcher_omp, test_task_run) {
-  constexpr int kSize = 250000;
-  std::vector<int> in(kSize);
+  std::vector<int> in = GenerateRndArray(kSize, kSeed);
   std::vector<int> out(in.size());
-  std::iota(in.rbegin(), in.rend(), 1);
 
   auto task_data_omp = std::make_shared<ppc::core::TaskData>();
   task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
@@ -68,4 +83,5 @@ TEST(korovin_n_qsort_batcher_omp, test_task_run) {
   auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_omp);
   perf_analyzer->TaskRun(perf_attr, perf_results);
   ppc::core::Perf::PrintPerfStatistic(perf_results);
+  ASSERT_TRUE(std::ranges::is_sorted(out.begin(), out.end()));
 }
