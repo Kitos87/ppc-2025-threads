69388eef1c946348efa3d29484fb50a0058b6ec7
Nikita Korovin <25564136+Kitos87@users.noreply.github.com>
2025-04-17 19:59:47 +0300

    davai

--- tasks/seq/korovin_n_qsort_batcher/perf_tests/main.cpp
+++ tasks/seq/korovin_n_qsort_batcher/perf_tests/main.cpp
@@ -4,7 +4,6 @@
 #include <chrono>
 #include <cstdint>
 #include <memory>
-#include <numeric>
 #include <random>
 #include <vector>
 
@@ -14,9 +13,9 @@
 
 namespace {
 constexpr int kSize = 250000;
-constexpr unsigned int kSeed = 25;
+constexpr int kSeed = 25;
 
-std::vector<int> GenerateRndArray(int size, unsigned int seed) {
+std::vector<int> GenerateRndArray(int size, int seed) {
   std::mt19937 gen(seed);
   std::uniform_int_distribution<int> dist(-1000, 1000);
 
@@ -29,25 +28,17 @@ std::vector<int> GenerateRndArray(int size, unsigned int seed) {
 }  // namespace
 
 TEST(korovin_n_qsort_batcher_seq, test_pipeline_run) {
-  // Create data
   std::vector<int> in = GenerateRndArray(kSize, kSeed);
   std::vector<int> out(in.size());
 
-  for (int i = 0; i < kSize; i++) {
-    in[i] = kSize - i;
-  }
-
-  // Create task_data
   auto task_data_seq = std::make_shared<ppc::core::TaskData>();
   task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
   task_data_seq->inputs_count.emplace_back(in.size());
   task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
   task_data_seq->outputs_count.emplace_back(out.size());
 
-  // Create Task
   auto test_task_sequential = std::make_shared<korovin_n_qsort_batcher_seq::TestTaskSequential>(task_data_seq);
 
-  // Create Perf attributes
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
   perf_attr->num_running = 10;
   const auto t0 = std::chrono::high_resolution_clock::now();
@@ -57,10 +48,8 @@ TEST(korovin_n_qsort_batcher_seq, test_pipeline_run) {
     return static_cast<double>(duration) * 1e-9;
   };
 
-  // Create and init perf results
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
 
-  // Create Perf analyzer
   auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_sequential);
   perf_analyzer->PipelineRun(perf_attr, perf_results);
   ppc::core::Perf::PrintPerfStatistic(perf_results);
@@ -68,11 +57,9 @@ TEST(korovin_n_qsort_batcher_seq, test_pipeline_run) {
 }
 
 TEST(korovin_n_qsort_batcher_seq, test_task_run) {
-  // Create data
   std::vector<int> in = GenerateRndArray(kSize, kSeed);
   std::vector<int> out(in.size());
 
-  // Create task_data
   auto task_data_seq = std::make_shared<ppc::core::TaskData>();
   task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
   task_data_seq->inputs_count.emplace_back(in.size());
