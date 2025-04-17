25c8da6e9a3b250e74262f6b06345a02eec2695d
Nikita Korovin <25564136+Kitos87@users.noreply.github.com>
2025-04-17 21:20:22 +0300

    1_500_000

--- tasks/omp/korovin_n_qsort_batcher/perf_tests/main.cpp
+++ tasks/omp/korovin_n_qsort_batcher/perf_tests/main.cpp
@@ -12,7 +12,7 @@
 #include "omp/korovin_n_qsort_batcher/include/ops_omp.hpp"
 
 namespace {
-constexpr int kSize = 250000;
+constexpr int kSize = 1500000;
 constexpr int kSeed = 25;
 
 std::vector<int> GenerateRndArray(int size, int seed) {
@@ -40,7 +40,7 @@ TEST(korovin_n_qsort_batcher_omp, test_pipeline_run) {
   auto test_task_omp = std::make_shared<korovin_n_qsort_batcher_omp::TestTaskOpenMP>(task_data_omp);
 
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
-  perf_attr->num_running = 35;
+  perf_attr->num_running = 10;
   const auto t0 = std::chrono::high_resolution_clock::now();
   perf_attr->current_timer = [&] {
     auto current_time_point = std::chrono::high_resolution_clock::now();
@@ -69,7 +69,7 @@ TEST(korovin_n_qsort_batcher_omp, test_task_run) {
   auto test_task_omp = std::make_shared<korovin_n_qsort_batcher_omp::TestTaskOpenMP>(task_data_omp);
 
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
-  perf_attr->num_running = 35;
+  perf_attr->num_running = 10;
   const auto t0 = std::chrono::high_resolution_clock::now();
   perf_attr->current_timer = [&] {
     auto current_time_point = std::chrono::high_resolution_clock::now();
